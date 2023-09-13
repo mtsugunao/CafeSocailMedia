4e67d50d807cc33c5a28b329bce5a62c51c6fe6e
tsugunao <onlymyrailgun10030326@gmail.com>
2023-09-13 16:37:06 -0700

    slightly changed search function

--- resources/views/cafeseeker.blade.php
+++ resources/views/cafeseeker.blade.php
@@ -8,7 +8,14 @@
 </head>
 <body>
     <h1>{{ $user->name }}</h1>
+    @if(Auth::user()->id !== $user->id)
     @livewire('follow', ['user' => $user])
+    @endif
+    @foreach($posts as $post)
+    <p>{{ $post->user->name }} {{ $post->created_at }}</p>
+    <p>{{ $post->cafe->name }}</p>
+    <p>{{ $post->content }}</p>
+    @endforeach
     
 </body>
 </html>
