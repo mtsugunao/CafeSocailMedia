faf267b561a629450862663e3500817bc1799fdb
tsugunao <onlymyrailgun10030326@gmail.com>
2023-11-15 14:12:03 -0800

    implemented google login

---
+++ app/Http/Controllers/GoogleLoginController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Exception;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Hash;
+
+class GoogleLoginController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        try {
+            $user = Socialite::driver('google')->user();
+            $finduser = User::where('google_id', $user->id)->first();
+
+            if($finduser){
+                Auth::login($finduser);
+                return redirect()->route('post.show')->with('feedback.success', "Successfully logged in!");
+            }else{
+                $newUser = User::create([
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'google_id' => $user->id,
+                ]);
+            }
+
+            Auth::login($newUser);
+
+            return redirect()->route('post.show')->with('feedback.success', "Successfully logged in!");
+        } catch (Exception $e) {
+            Log::error($e);
+            throw $e;
+        }
+    }
+}
+
