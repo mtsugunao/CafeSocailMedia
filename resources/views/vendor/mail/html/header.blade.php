f850161af7954946c65be8fba7dd4caef78d2e9f
tsugunao <onlymyrailgun10030326@gmail.com>
2023-11-20 17:02:02 -0800

    added logo.png

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="{{ asset('images/mugnet-logo.svg') }}" class="logo" alt="MugNet Logo">
+            <img src="{{ asset('images/MugNet.png') }}" class="logo" alt="MugNet Logo">
         </a>
     </td>
 </tr>
